1f2cf13fa3f73816db72a47b7cad184680feba50
Jonathan Finlay <jfinlay24@yahoo.ie>
2020-09-24 18:35:56 +0100

    dropdown populates data

--- src/components/PizzaChoice.js
+++ src/components/PizzaChoice.js
@@ -3,44 +3,42 @@ import React from "react"
 class PizzaChoice extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { value: "Select Pizza" };
-    
+        this.state = {
+            value: "Select Pizza",
+        };
+
         // this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleSubmit(event) {
+    }
+
+    handleSubmit(event) {
         alert("Your pizza choice is: " + this.state.value);
         event.preventDefault();
-      }
-    
-      handleChange = event => {
+    }
+
+    handleChange = event => {
         this.setState({ value: event.target.value });
-      };
-    
+    };
+
     render() {
-      return (
-        // <div>
-        //   {this.props.menu.map(menu => (
-        //     <li key={menu.id}>{menu.title}</li>
-        //   ))}
-        // </div>
-        <form onSubmit={this.handleSubmit}>
-            <label>
-            Pick a pizza:
-            <select value={this.state.value} onChange={this.handleChange}>
-                <option value="Small">Small</option>
-                <option value="Plain">Plain</option>
-                <option value="Pizza">Pizza</option>
-                <option value="Please">Please</option>
-            </select>
-            </label>
-            <input type="submit" value="Submit" />
-        </form>
-      
-      
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Pick a pizza:
+                    <select>
+                        {this.props.menu.map(menu => (
+                            <option key={menu.id} value={menu.title}>
+                                {menu.title}
+                            </option>
+                        ))}
+                    </select>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+
+
         )
     }
-  }
-  
-  export default PizzaChoice
+}
+
+export default PizzaChoice
